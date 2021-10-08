bb4c892144f5d32c6eff5d45ef4f9e582374df55
Chernyackoff <chernyackoff.vlad@yandex.ru>
2021-10-08 21:59:32 +0300

    initialized development of backend part

--- server/server.py
+++ server/server.py
@@ -2,11 +2,32 @@ from flask import Flask
 import firebase_admin
 from firebase_admin import credentials
 from pathlib import Path
+from flask_restful import Resource, Api
+import os
+import multiprocessing as mp
 
 cred = credentials.Certificate(Path(__file__).parent / "key/vtb-hackathon-firebase-adminsdk-mh10o-0e7b464d7d.json")
 firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
+api = Api(app)
+
+
+class Feature(Resource):
+    def get(self, data):
+        code = data["data"]["code"]
+
+    def get_ds(self, id: str):
+        pass
+
+    def execute_code(self, code: str, data_set):
+        with open('executables.py', 'w') as f:
+            f.writelines(code.split('\n'))
+            f.close()
+
+
+
+api.add_resource(Feature, '/api/<string: data>')
 
 
 @app.route("/test")
