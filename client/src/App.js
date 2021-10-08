2859acca616ee8dcdfa3d0a17098a8235cbcd24f
BuildTools <unconfigured@null.spigotmc.org>
2021-10-08 22:45:36 +0300

    ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
 import { initializeApp } from "firebase/app";
-import Editor, { useMonaco } from "@monaco-editor/react";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCpkWpy-HyuAodtrWajEE6_4ByOq_GtpAI",
@@ -16,31 +15,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 function App() {
-  const monaco = useMonaco();
-  const beforeUserCode = "import pandas as pd\n"
-  const inbetweenMessage = "# Здесь можете ввести нужные преобразования"
-  const afterUserCode = "\nreturn df['feature']"
-
-  useEffect(() => {
-    if (monaco) {
-      console.log("here is the monaco instance:", monaco.editor);
-    }
-  }, [monaco]);
-  function handleEditorChange(value, event) {
-    if (!(value.startsWith(beforeUserCode) && value.endsWith(afterUserCode))) {
-      monaco.editor.getModels()[0].undo()
-    }
-    console.log("here is the current model value:", value);
-  }
   return ( 
     <div>
-      <Editor
-        height="90vh"
-        defaultLanguage="python"
-        defaultValue={beforeUserCode + inbetweenMessage + afterUserCode}
-        theme="vs-dark"
-        onChange={handleEditorChange}
-      />
+      EMPTY
     </div>
    );
 }
