48158d225f63c248a5505b4f3dda5647c5d3d7a5
sort-nlogn <artomstepanov443@gmail.com>
2021-10-09 00:21:27 +0300

    хуйня-из-под-коня

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
-import { initializeApp } from "firebase/app";
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+import UploadButton from './Components/UploadButton/UploadButton'
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCpkWpy-HyuAodtrWajEE6_4ByOq_GtpAI",
@@ -12,27 +21,52 @@ const firebaseConfig = {
   measurementId: "G-H7506F30MH"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+const storage = firebase.storage()
 
 function App() {
-  const [test, setTest] = useState();
-  useEffect(() => {
-    fetch("/test").then(
-      res => res.json()
-    ).then(
-      test => {
-        setTest(test)
-        console.log(test)
-      }
-    )
-  }, []);
-  return ( 
+  const [user] = useAuthState(auth)
+
+  return(
+    <div>
+      <header>
+
+      </header>
+      <section>
+        {user ? 
+        <FilesList/> : <SignIn/>}
+      </section>
+    </div>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider)
+  }
+
+  return (  
+    <button onClick={ signInWithGoogle }>Sign In with Google</button>
+  );
+}
+
+function FilesList() {
+
+  return (  
     <div>
       {
-        test?.shit
+        auth.currentUser && ( 
+          <button onClick={() => auth.signOut()}>Sign Out</button>
+        )
       }
+      <UploadButton userId={auth.currentUser.uid}/>
+     { auth.currentUser.email}
     </div>
-   );
+  );
 }
 
 export default App;
