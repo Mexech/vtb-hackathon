77de61a8e6fb9c18397eecb2a95732a5d66b4cd0
BuildTools <unconfigured@null.spigotmc.org>
2021-10-08 22:16:16 +0300

    Added editing in range

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import { initializeApp } from "firebase/app";
+import Editor, { useMonaco } from "@monaco-editor/react";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCpkWpy-HyuAodtrWajEE6_4ByOq_GtpAI",
@@ -15,22 +16,31 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 function App() {
-  const [test, setTest] = useState();
+  const monaco = useMonaco();
+  const beforeUserCode = "import pandas as pd\n"
+  const inbetweenMessage = "# Здесь можете ввести нужные преобразования"
+  const afterUserCode = "\nreturn df['feature']"
+
   useEffect(() => {
-    fetch("/test").then(
-      res => res.json()
-    ).then(
-      test => {
-        setTest(test)
-        console.log(test)
-      }
-    )
-  }, []);
+    if (monaco) {
+      console.log("here is the monaco instance:", monaco.editor);
+    }
+  }, [monaco]);
+  function handleEditorChange(value, event) {
+    if (!(value.startsWith(beforeUserCode) && value.endsWith(afterUserCode))) {
+      monaco.editor.getModels()[0].undo()
+    }
+    console.log("here is the current model value:", value);
+  }
   return ( 
     <div>
-      {
-        test?.shit
-      }
+      <Editor
+        height="90vh"
+        defaultLanguage="python"
+        defaultValue={beforeUserCode + inbetweenMessage + afterUserCode}
+        theme="vs-dark"
+        onChange={handleEditorChange}
+      />
     </div>
    );
 }
