f25d414fff84bbf21c1e77fc316594b68bfab3a6
Chernyackoff <chernyackoff.vlad@yandex.ru>
2021-10-09 09:46:55 +0300

    Implemented firebase api to get data sets. Main func is done, but not tested

--- server/server.py
+++ server/server.py
@@ -1,15 +1,19 @@
 from flask import Flask
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, storage
 from pathlib import Path
 from flask_restful import Resource, Api
-import os
+import os, json
 from multiprocessing import Process, Queue
+from io import StringIO
+import pandas as pd
 from pandas import DataFrame
 import runner
 
 cred = credentials.Certificate(Path(__file__).parent / "key/vtb-hackathon-firebase-adminsdk-mh10o-0e7b464d7d.json")
-firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'vtb-hackathon.appspot.com'
+})
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,19 +21,38 @@ api = Api(app)
 
 class Feature(Resource):
     def get(self, data):
+        data = json.load(data)
         code = data["data"]["code"]
+        uid = data["uid"]
+        filename = data["data"]["filename"]
 
-    def get_ds(self, id: str):
-        pass
+        d_frame = self.__get_df(uid, filename)
 
-    def execute_code(self, code: str, data_set: DataFrame):
+        result = self.__execute_code(code, d_frame)
+
+        return json.dump(result)
+
+    def __get_df(self, uid: str, filename: str):
+        # Getting data set from url
+        raw_csv = storage.bucket().blob(uid + '/' + filename).download_as_text()
+
+        # Refactoring to data frame
+        f = StringIO(raw_csv)
+        df = pd.read_csv(f, sep=",")
+        return df
+
+    def __execute_code(self, code: str, data_set: DataFrame):
+
+        # Put code from frontend into executable python file
         with open('runner.py', 'w') as f:
             f.writelines(code.split('\n'))
             f.close()
 
+        # Generates Queue object to manage data set and results of inner code
         queue = Queue()
         queue.put(data_set)
 
+        # Making process for execution of inner code file
         p = Process(target=runner.run, args=(queue,))
         p.start()
         p.join()
@@ -42,7 +65,7 @@ api.add_resource(Feature, '/api/<string:data>')
 
 @app.route("/test")
 def test():
-    return {"shit": ["piss1", "piss2"]}
+    return {"test": ["res1", "res2"]}
 
 
 if __name__ == "__main__":
