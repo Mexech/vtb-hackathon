b80e2fda7080a2b76000b6b64e07e4b081c54c38
Chernyackoff <chernyackoff.vlad@yandex.ru>
2021-10-09 09:20:41 +0300

    Added execution of inner code

--- server/server.py
+++ server/server.py
@@ -4,7 +4,9 @@ from firebase_admin import credentials
 from pathlib import Path
 from flask_restful import Resource, Api
 import os
-import multiprocessing as mp
+from multiprocessing import Process, Queue
+from pandas import DataFrame
+import runner
 
 cred = credentials.Certificate(Path(__file__).parent / "key/vtb-hackathon-firebase-adminsdk-mh10o-0e7b464d7d.json")
 firebase_admin.initialize_app(cred)
@@ -20,14 +22,22 @@ class Feature(Resource):
     def get_ds(self, id: str):
         pass
 
-    def execute_code(self, code: str, data_set):
-        with open('executables.py', 'w') as f:
+    def execute_code(self, code: str, data_set: DataFrame):
+        with open('runner.py', 'w') as f:
             f.writelines(code.split('\n'))
             f.close()
 
+        queue = Queue()
+        queue.put(data_set)
+
+        p = Process(target=runner.run, args=(queue,))
+        p.start()
+        p.join()
+
+        return queue.get()
 
 
-api.add_resource(Feature, '/api/<string: data>')
+api.add_resource(Feature, '/api/<string:data>')
 
 
 @app.route("/test")
