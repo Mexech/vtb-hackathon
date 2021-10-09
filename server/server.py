991e05e07ad0ad6d5ecb742b93519b6616bf7552
Chernyackoff <chernyackoff.vlad@yandex.ru>
2021-10-09 19:37:27 +0300

    NewFeature is done but not tested

--- server/server.py
+++ server/server.py
@@ -6,12 +6,11 @@ import numpy as np
 from io import StringIO
 from firebase_admin import credentials, storage
 from pathlib import Path
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 import os, json
 from multiprocessing import Process, Queue
 from io import StringIO
 import pandas as pd
-
 
 cred = credentials.Certificate(Path(__file__).parent / "key/vtb-hackathon-firebase-adminsdk-mh10o-0e7b464d7d.json")
 firebase_admin.initialize_app(cred, {
@@ -24,7 +23,7 @@ api = Api(app)
 
 class Feature(Resource):
     def get(self):
-        data = request.get_json(force=True)
+        data = request.json
         code = data["data"]["code"]
         uid = data["uid"]
         filename = data["data"]["filename"]
@@ -44,12 +43,10 @@ class Feature(Resource):
         df = pd.read_csv(f, sep=",")
         return df
 
-    def __execute_code(self, code: str, data_set: DataFrame):
-
+    def __execute_code(self, code: str, data_set: pd.DataFrame):
         # Put code from frontend into executable python file
         with open('runner.py', mode='w') as f:
             f.writelines(code)
-
 
         # Generates Queue object to manage data set and results of inner code
         queue = Queue()
@@ -66,7 +63,59 @@ class Feature(Resource):
         return queue.get()
 
 
-api.add_resource(Feature, '/api')
+# NewFeature creating
+
+nf_parser = reqparse.RequestParser()
+nf_parser.add_argument('uid', type=str, help="User ID for link")
+nf_parser.add_argument('link_src', type=str, help="Name of source table")
+nf_parser.add_argument('link_dst', type=str, help="Name of destination table")
+nf_parser.add_argument('features', help="List of features from src table")
+
+
+class NewFeature(Resource):
+    def post(self):
+        args = nf_parser.parse_args()
+        uid = args["uid"]
+        link_src = uid + '/' + args["link_src"]
+        link_dst = uid + '/' + args["link_dst"]
+        features = args["features"]
+        new_ftrs = self.get_features(link_src, features)
+        if e := self.append_features(link_dst, new_ftrs):
+            return {"result": e}
+        else:
+            return {"result": "Success"}
+
+    def get_features(self, link, features):
+        raw_csv = storage.bucket().blob(link).download_as_text()
+
+        # Refactoring to data frame
+        f = StringIO(raw_csv)
+        df = pd.read_csv(f, sep=",")
+
+        result = []
+        for ftr in features:
+            result.append(df[ftr])
+
+        return result
+
+    def append_features(self, link, features):
+        try:
+            raw_csv = storage.bucket().blob(link).download_as_text()
+            f = StringIO(raw_csv)
+            df = pd.read_csv(f, sep=",")
+            for ftr in features:
+                df.append(ftr)
+
+            storage.bucket().blob(link).upload_from_string(df.to_csv())
+            return None
+        except Exception as e:
+            print(f"Something gone wrong! {e}")
+            return e
+
+
+api.add_resource(Feature, '/api/code')
+api.add_resource(NewFeature, '/api/new-feature')
+
 
 @app.route("/test")
 def test():
@@ -75,15 +124,15 @@ def test():
 
 @app.route("/api/getmetadata/<uid>/<filename>")
 def get_metadata(uid, filename):
-    raw_csv = storage.bucket().blob(uid+'/'+filename).download_as_text()
+    raw_csv = storage.bucket().blob(uid + '/' + filename).download_as_text()
     f = StringIO(raw_csv)
     df = pd.read_csv(f, sep=",")
     return json.dumps({"rows": df.shape[0]})
 
-    
+
 @app.route("/api/getdataset/<uid>/<filename>")
 def get_dataset(uid, filename):
-    raw_csv = storage.bucket().blob(uid+'/'+filename).download_as_text()
+    raw_csv = storage.bucket().blob(uid + '/' + filename).download_as_text()
     f = StringIO(raw_csv)
     df = pd.read_csv(f, sep=",")
     data = df.head().to_dict("records")
@@ -91,4 +140,4 @@ def get_dataset(uid, filename):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True)
